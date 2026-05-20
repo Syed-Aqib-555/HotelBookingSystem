a2999c209bd6c5e07308b5c4296cce192f5af16c
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-05-20 05:15:40 +0500

    Add generic repository interface with asynchronous CRUD operations

--- HotelBookingSystem/Repositories/IRepository.cs
+++ HotelBookingSystem/Repositories/IRepository.cs
@@ -5,7 +5,6 @@ namespace HotelBookingSystem.Repositories;
 public interface IRepository<T> where T : BaseEntity
 {
     Task<List<T>> GetAllAsync();
-
     Task<T?> GetByIdAsync(int id);
 
     Task AddAsync(T entity);
