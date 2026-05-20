821f1d3cde4a3e92f3a5a3aaab9c90d05cca06a1
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-05-20 05:16:21 +0500

    Define booking repository interface for user, admin, and detailed booking queries

--- HotelBookingSystem/Repositories/IBookingRepository.cs
+++ HotelBookingSystem/Repositories/IBookingRepository.cs
@@ -5,7 +5,6 @@ namespace HotelBookingSystem.Repositories;
 public interface IBookingRepository : IRepository<Booking>
 {
     Task<List<Booking>> GetBookingsForUserAsync(int userId);
-
     Task<List<Booking>> GetBookingsForAdminAsync();
 
     Task<Booking?> GetBookingDetailsAsync(int id);
