abb365850b0e85dd6807b145aac7c8bc19e46aa6
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-05-20 05:29:02 +0500

    Add AppDbContext with DbSets for rooms and bookings

--- HotelBookingSystem/Data/AppDbContext.cs
+++ HotelBookingSystem/Data/AppDbContext.cs
@@ -8,7 +8,6 @@ namespace HotelBookingSystem.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Booking> Bookings { get; set; }
     }
